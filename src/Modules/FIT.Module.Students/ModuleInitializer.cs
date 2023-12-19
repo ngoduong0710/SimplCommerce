69138ab9bbde77167d5bcbfd4fdc6fea89b790ca
QuyenPN <pnquyen.nvla2.2019@gmail.com>
2023-12-19 21:41:11 +0700

    Create student module

---
+++ src/Modules/FIT.Module.Students/ModuleInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using SimplCommerce.Infrastructure;
+using SimplCommerce.Infrastructure.Modules;
+
+namespace FIT.Module.Students
+{
+    public class ModuleInitializer : IModuleInitializer
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            GlobalConfiguration.RegisterAngularModule("simplAdmin.student");
+
+        }
+    }
+}
