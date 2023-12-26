5a5d3c9ba83bdca9958ad6606dbb39ae958db564
2bngbtran04 <145552140+2bngbtran04@users.noreply.github.com>
2023-12-26 22:32:12 +0700

    Tạo thử file Angular Typescript trong thư mục Views\Student\ViewStudent.ts

---
+++ src/SimplCommerce.WebHost/Views/Student/ViewStudent.ts
@@ -0,0 +1,29 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'app-student-list',
+    template: `
+    <h2>Student List</h2>
+    <ul>
+      <li *ngFor="let student of students">
+        {{ student.name }} - {{ student.grade }}
+      </li>
+    </ul>
+  `,
+    styles: [`
+    /* Add your custom styles here */
+  `]
+})
+export class StudentListComponent implements OnInit {
+    students: any[];
+
+    constructor(private http: HttpClient) { }
+
+    ngOnInit(): void {
+        this.http.get<any[]>('https://localhost:49206/api/student/get-all')
+            .subscribe(data => {
+                this.students = data;
+            });
+    }
+}
